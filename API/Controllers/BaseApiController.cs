baab0af54dce0cd552437817b3c8429b3fcdd8d4
Renan Acuin <racuin2593@gmail.com>
2023-12-12 17:52:52 +0800

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,15 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        public IMediator _mediator;
 
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
     }
 }
