fbf766957e9179919e995560334d51aaa64b1f13
Renan Acuin <racuin2593@gmail.com>
2023-12-13 17:23:32 +0800

    End of section 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import App from "./app/layout/App";
 import "./app/layout/styles.css";
+import { StoreContext, store } from "./app/stores/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <StoreContext.Provider value={store}>
     <App />
-  </React.StrictMode>
+  </StoreContext.Provider>
+  // </React.StrictMode>
 );
