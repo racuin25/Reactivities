f96d36a4f4231e87c3437dc5ccb1f3652e4c1ac6
Renan Acuin <racuin2593@gmail.com>
2023-12-28 23:04:25 +0800

    End of section 18

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class Profiles : BaseApiController
+    public class ProfilesController : BaseApiController
     {
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
